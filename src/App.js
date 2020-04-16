ae32dc36cfea3a92d90d5d2ae26618ea38202acf
NahomKibreab <nknahom@gmail.com>
2020-04-15 18:55:29 -0600

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import FetchJson from "./component/FetchJson";
 import { Route, Switch, Redirect } from "react-router-dom";
 import About from "./component/About";
+import Navigation from "./component/Navigation";
 
 function App() {
   return (
     <div>
+      <Navigation />
       <Switch>
         <Route path="/about" component={About} />
         <Route path="/" component={FetchJson} />
