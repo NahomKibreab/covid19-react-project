ce6c8c07dfada313aca58d0241aa45d063d34c6f
NahomKibreab <nknahom@gmail.com>
2020-04-24 19:18:24 -0600

    Updates

---
+++ precache-manifest.e4f973834e0ff9e54b7c102490ad8f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf752e464f7123448c16aaee4912899",
+    "url": "/covid19-react-project/index.html"
+  },
+  {
+    "revision": "71cee827f4df34eae7e3",
+    "url": "/covid19-react-project/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "d05ca704a76206e749d6",
+    "url": "/covid19-react-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "71cee827f4df34eae7e3",
+    "url": "/covid19-react-project/static/js/2.bfe64950.chunk.js"
+  },
+  {
+    "revision": "3bf19f0850ca84d44b0b74bfe033219f",
+    "url": "/covid19-react-project/static/js/2.bfe64950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05ca704a76206e749d6",
+    "url": "/covid19-react-project/static/js/main.0902dd31.chunk.js"
+  },
+  {
+    "revision": "c524487a89fb545d8abd",
+    "url": "/covid19-react-project/static/js/runtime-main.ea739c65.js"
+  }
+]);
