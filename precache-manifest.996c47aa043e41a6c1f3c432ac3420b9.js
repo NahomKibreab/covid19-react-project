75573a409bf09f6177c987aab5aa7029c99f89b5
NahomKibreab <nknahom@gmail.com>
2020-05-04 00:24:11 -0600

    Updates

---
+++ precache-manifest.996c47aa043e41a6c1f3c432ac3420b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbae3adb611dad0678083b61d7a9e9b",
+    "url": "/covid19-react-project/index.html"
+  },
+  {
+    "revision": "231535c2f8ae5ac9ab6e",
+    "url": "/covid19-react-project/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "e3bb92bc82ead31967d5",
+    "url": "/covid19-react-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "231535c2f8ae5ac9ab6e",
+    "url": "/covid19-react-project/static/js/2.6ea0bd05.chunk.js"
+  },
+  {
+    "revision": "f825d1ecc95979ab3c6b8d6608a5ea5d",
+    "url": "/covid19-react-project/static/js/2.6ea0bd05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3bb92bc82ead31967d5",
+    "url": "/covid19-react-project/static/js/main.f07b0e52.chunk.js"
+  },
+  {
+    "revision": "c524487a89fb545d8abd",
+    "url": "/covid19-react-project/static/js/runtime-main.ea739c65.js"
+  },
+  {
+    "revision": "cba02cc17d6970dc9a8b5c239cc9e855",
+    "url": "/covid19-react-project/static/media/nknahom_pro.cba02cc1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-react-project/precache-manifest.4ffc958e87451b385e389de096e80537.js"
+  "/covid19-react-project/precache-manifest.996c47aa043e41a6c1f3c432ac3420b9.js"
 );
 
 self.addEventListener('message', (event) => {
